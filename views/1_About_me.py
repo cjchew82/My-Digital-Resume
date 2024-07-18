608712f55250c6125ab0f518c2d7ba7448eccf65
cjchew82 <ccjuen@gmail.com>
2024-07-18 12:51:32 +0800

    Update 1_About_me.py

--- views/1_About_me.py
+++ views/1_About_me.py
@@ -84,7 +84,7 @@ profile_pic = Image.open(profile_pic)
 cols = st.columns(2, gap='small')
 
 with cols[0]:
-    st.image(profile_pic, width=230)
+    st.image(profile_pic, width=400)
     # st.image(profile_pic, width=230)
     # st.image(profile_pic, use_column_width=False, width=230, output_format="PNG", caption="Profile Picture", clamp=True, class_="profile-pic")
 
